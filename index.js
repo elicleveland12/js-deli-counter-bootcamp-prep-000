c42d2143ac7dc321e47fcc338c5959efc94c98a8
Eli Cleveland <elicleveland12@gmail.com>
2018-01-31 18:16:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   while (katzDeliLine.length > 0) {
-    return "Currently serving " + --katzDeliLine.length + ".";
+    return "Currently serving " + --katzDeliLine[i] + ".";
   }
   return "There is nobody waiting to be served!";
 }
