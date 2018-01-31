444a4be91550a2aee5a1b6e3c045ffc176a0052d
Eli Cleveland <elicleveland12@gmail.com>
2018-01-31 18:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } 
+  else {
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      
+    }
+  }
 }
 
 
