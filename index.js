aac9437be499c8879c801d79b536bb8e4e608e3e
Eli Cleveland <elicleveland12@gmail.com>
2018-01-31 17:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   for (let i = 0; i < katzDeliLine.length; --i) {
-    if (katzDeliLine === 0) {
+    if (katzDeliLine !== null) {
       return "Currently serving " + katzDeliLine[0] + ".";
     }
     else {
