239e645c8cb3c3a3fad60996fe135b5d70bd44d0
Eli Cleveland <elicleveland12@gmail.com>
2018-12-10 10:30:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-var katzDeli = [];
 var katzDeliLine = [];
+var ticketNumber
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+function takeANumber(katzDeliLine) {
+  katzDeliLine.push(ticketNumber++);
+  return "You are number " + ticketNumber + " in line.";
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    var name = katzDeliLine[0];
-    katzDeliLine.splice(0, 1);
-    return "Currently serving " + name + ".";
+    return "Currently serving " + katzDeliLine.shift() + ".";
   }
 }
 
@@ -23,9 +21,10 @@ function currentLine(katzDeliLine) {
   } 
   else {
     for(var i = 0; i < katzDeliLine.length; i++) {
-      names += (i + 1) + ". " + katzDeliLine[i] + ", ";
+      names += katzDeliLine[i] + ", ";
     }
     names = names.slice(0, names.length-2);
+    //console.log("The line is currently: " + names);
     return "The line is currently: " + names; 
   }
 }
